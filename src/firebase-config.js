3d09bdb74e0e954350a6573ef09b05b2216551c3
saehwan <tpghks298@naver.com>
2023-06-07 15:52:55 -0500

    made login page

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDew-TKwfdIO3q1sZ4P-1BlVVelVktDTfo",
+    authDomain: "movie-review-a4dd2.firebaseapp.com",
+    projectId: "movie-review-a4dd2",
+    storageBucket: "movie-review-a4dd2.appspot.com",
+    messagingSenderId: "329380250954",
+    appId: "1:329380250954:web:f96d0d6d8c41b830706db3",
+    measurementId: "G-M31YBM5RD0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
